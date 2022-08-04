3292a568354893d4054bdb395c5f0b853529f490
Hong Minhee <hong.minhee@gmail.com>
2022-08-05 02:21:28 +0900

    WezTerm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require "wezterm"
+return {
+  font = wezterm.font { family = "Fira Code", weight = "Light" },
+  font_size = 16,
+
+  color_scheme = "OneDark (base16)",
+  window_frame = { font_size = 14 },
+}
