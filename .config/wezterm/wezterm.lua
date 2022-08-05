6c86680f950494ff2ef4261e9be87700e560d5db
Hong Minhee <hong.minhee@gmail.com>
2022-08-05 13:24:55 +0900

    Cmd-K to clear scrollback on WezTerm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,8 +1,20 @@
 local wezterm = require "wezterm"
+local act = wezterm.action
+
 return {
   font = wezterm.font { family = "Fira Code", weight = "Light" },
   font_size = 16,
 
   color_scheme = "OneDark (base16)",
   window_frame = { font_size = 14 },
+
+  keys = {
+    {
+      key = 'k',
+      mods = 'CMD',
+      action = act.ClearScrollback "ScrollbackAndViewport",
+    },
+  },
 }
+
+-- vim: set et sw=2 ts=2 sts=2 ft=lua:
