595491e775c2927b8a01d7b3be57c42f379e036b
Hong Minhee <hong@minhee.org>
2023-11-21 00:04:52 +0900

    Tell pwsh to tell the terminal about its CWD

--- PSProfile/profile.ps1
+++ PSProfile/profile.ps1
@@ -9,3 +9,14 @@ if (Get-Command -ErrorAction SilentlyContinue /opt/homebrew/bin/brew) {
 if (Get-Command -ErrorAction SilentlyContinue starship) {
  (&starship init powershell) | Invoke-Expression
 }
+
+# https://learn.microsoft.com/en-us/windows/terminal/tutorials/new-tab-same-directory#powershell-with-starship
+function Invoke-Starship-PreCommand {
+  $loc = $executionContext.SessionState.Path.CurrentLocation;
+  $prompt = "$([char]27)]9;12$([char]7)"
+  if ($loc.Provider.Name -eq "FileSystem")
+  {
+    $prompt += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+  }
+  $host.ui.Write($prompt)
+}
